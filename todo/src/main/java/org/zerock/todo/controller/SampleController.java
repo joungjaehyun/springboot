4bf2d4c6d483e6a9340e901694c35eb13f3882e4
joungjaehyun <joungjaehyun1008@gmail.com>
2023-06-08 22:18:37 +0900

    MakeTodo

---
+++ todo/src/main/java/org/zerock/todo/controller/SampleController.java
@@ -0,0 +1,18 @@
+package org.zerock.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/sample/")
+@Log4j2
+public class SampleController {
+
+    @GetMapping({"/hello","/list"})
+    public void hello(){
+        log.info("sample/hello..................");
+    }
+}
