bb5eaf22106380522c8244e62354b91bcae37469
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-20 16:40:50 +0900

    service transaction

--- b4/src/main/java/org/zerock/b4/mappers/ProductMapper.java
+++ b4/src/main/java/org/zerock/b4/mappers/ProductMapper.java
@@ -1,6 +1,7 @@
 package org.zerock.b4.mappers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.zerock.b4.dto.PageRequestDTO;
 import org.zerock.b4.dto.ProductListDTO;
@@ -8,4 +9,8 @@ import org.zerock.b4.dto.ProductListDTO;
 public interface ProductMapper {
     
     List<ProductListDTO> getList(PageRequestDTO pageRequestDTO);
+
+    int insertProduct(ProductListDTO productListDTO);
+
+    int insertImages(List<Map<String,String>> imageMap);
 }
