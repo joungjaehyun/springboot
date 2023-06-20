c537b2ad40f0e07dcf933292a52ab8353f68239f
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-20 17:23:04 +0900

    lamda insertTest

--- b4/src/main/java/org/zerock/b4/dto/ProductRegisterDTO.java
+++ b4/src/main/java/org/zerock/b4/dto/ProductRegisterDTO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 public class ProductRegisterDTO {
 
+    private Integer pno;
     private String pname;
     private int price;
     private boolean status;
--- b4/src/main/java/org/zerock/b4/mappers/ProductMapper.java
+++ b4/src/main/java/org/zerock/b4/mappers/ProductMapper.java
@@ -5,12 +5,13 @@ import java.util.Map;
 
 import org.zerock.b4.dto.PageRequestDTO;
 import org.zerock.b4.dto.ProductListDTO;
+import org.zerock.b4.dto.ProductRegisterDTO;
 
 public interface ProductMapper {
     
     List<ProductListDTO> getList(PageRequestDTO pageRequestDTO);
 
-    int insertProduct(ProductListDTO productListDTO);
+    int insertProduct(ProductRegisterDTO registerDTO);
 
     int insertImages(List<Map<String,String>> imageMap);
 }
