3507b41369a2b0c0b57527a3b444ae84b7d53617
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-20 15:23:41 +0900

    컨트롤러 체크

--- b4/src/main/java/org/zerock/b4/controller/ProductController.java
+++ b4/src/main/java/org/zerock/b4/controller/ProductController.java
@@ -2,7 +2,9 @@ package org.zerock.b4.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.b4.dto.ProductRegisterDTO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,5 +18,13 @@ public class ProductController {
     @GetMapping("/register")
     public void register(){
         log.info("get product register");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(ProductRegisterDTO registerDTO){
+
+        log.info("-------------------");
+        log.info(registerDTO);
+        return "redirect:/product/list";
     }
 }
---
+++ b4/src/main/java/org/zerock/b4/dto/ProductRegisterDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.b4.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ProductRegisterDTO {
+
+    private String pname;
+    private int price;
+    private boolean status;
+
+    // uuid와 파일네임이 필요하다.
+    private List<String>fileNames;
+    
+}
