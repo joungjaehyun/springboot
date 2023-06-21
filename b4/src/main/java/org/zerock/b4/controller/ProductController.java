4c67bf0c68c97487a4455d6d552d85a3a36be971
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-21 10:56:31 +0900

    Service Imple

--- b4/src/main/java/org/zerock/b4/controller/ProductController.java
+++ b4/src/main/java/org/zerock/b4/controller/ProductController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b4.dto.ProductRegisterDTO;
+import org.zerock.b4.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -15,16 +17,26 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/product")
 public class ProductController {
     
+    private final ProductService productService;
+
     @GetMapping("/register")
     public void register(){
         log.info("get product register");
     }
 
     @PostMapping("/register")
-    public String registerPost(ProductRegisterDTO registerDTO){
+    public String registerPost(ProductRegisterDTO registerDTO, RedirectAttributes rttr){
 
         log.info("-------------------");
         log.info(registerDTO);
+
+        Integer pno = productService.register(registerDTO);
+
+        log.info("NWE PNO: " + pno);
+        
+        rttr.addFlashAttribute("result", pno);
+        //redirect는 오직 get방식만
+
         return "redirect:/product/list";
     }
 }
