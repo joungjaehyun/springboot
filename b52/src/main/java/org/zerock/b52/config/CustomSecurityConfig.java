b6eba1006a327f1235d253f16e0a8228352f297b
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-23 15:37:37 +0900

    cookie db setting

--- b52/src/main/java/org/zerock/b52/config/CustomSecurityConfig.java
+++ b52/src/main/java/org/zerock/b52/config/CustomSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.zerock.b52.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,21 +10,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Configuration
 @Log4j2
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
-    
+
+    // DB에 쿠키값을 넣기 위해서 datasource 설정
+    private final DataSource dataSource;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
 
         log.info("filte chain----------------------------------------");
 
@@ -37,19 +53,19 @@ public class CustomSecurityConfig {
         // tokenValiditySeconds -> 쿠기의 유효시간 설정 초단위임
         // 지금 새창으로 열때는 아예 작동이 안된다 왜냐면
         // 지금 저장할방법을 안정해놨기때문
-        http.rememberMe(config->{
-
-            config.tokenValiditySeconds(60*60*24*7);
+        // network response 영역에 set rememberme 해줌
+        http.rememberMe(config -> {
+            config.tokenRepository(persistentTokenRepository());
+            config.tokenValiditySeconds(60 * 60 * 24 * 7);
         });
-
 
         // default()에는 _csrf 이름의 input 태그가 hidden으로 숨어있다.
         // disable()을쓰면 csrf 태그가 사라진다.
         http.csrf(config -> {
-        config.disable();
+            config.disable();
         });
 
         return http.build();
-    
+
     }
 }
